cddeba8a108c9542a090887fd7976ef115d5e303
JTRMinsk <jtrminsk@yahoo.com>
2021-02-18 00:00:22 +0800

    upload file

--- FileUploader/src/main/java/org/salim/FileUploader.java
+++ FileUploader/src/main/java/org/salim/FileUploader.java
@@ -7,11 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileUploader {
     private static final Logger LOG = LoggerFactory.getLogger(FileUploader.class);
+    private static final String remoteRootPath = "/usr/local/filerepo";
+    private static final String localRootPath = "D:\\filerepo\\";
 
     private static void checkFile (List<File> fileList, File file) {
         if (file.isFile()) {
@@ -24,6 +28,23 @@ public class FileUploader {
             }
         }
     }
+
+    private static void upload (FTPClient ftpClient, String ftpRootDirectory, File localFile) throws IOException {
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+
+        if (!ftpClient.changeWorkingDirectory(ftpRootDirectory)) {
+            ftpClient.makeDirectory(ftpRootDirectory);
+            ftpClient.changeWorkingDirectory(ftpRootDirectory);
+        }
+
+        FileInputStream fileInputStream = new FileInputStream(localFile);
+
+
+
+    }
+
+
 
     public static void main(String[] args) throws Exception{
         FTPClient ftpClient = new FTPClient();
@@ -39,10 +60,15 @@ public class FileUploader {
             LOG.info("Got Connection");
         } else {
             LOG.info("No Connection");
+            return;
+        }
+        if (!ftpClient.changeWorkingDirectory(remoteRootPath)) {
+            ftpClient.makeDirectory(remoteRootPath);
+            ftpClient.changeWorkingDirectory(remoteRootPath);
         }
 
         //local files
-        File localFileBase = new File("D:\\filerepo\\");
+        File localFileBase = new File(localRootPath);
         if (localFileBase.exists() && localFileBase.isDirectory()) {
             //遍历，推荐用递归,列出所有文件
             List<File> allFiles = new ArrayList<>();
@@ -53,7 +79,8 @@ public class FileUploader {
             LOG.error("This path not exists or not directory");
         }
 
-        //remote files
+        //remote files, last modify date
+
 
 
     }
