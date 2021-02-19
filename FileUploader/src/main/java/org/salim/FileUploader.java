6866c12df39c33c0d17dbd276f4260453851a626
JTRMinsk <jtrminsk@yahoo.com>
2021-02-19 11:41:30 +0800

    upload success

--- FileUploader/src/main/java/org/salim/FileUploader.java
+++ FileUploader/src/main/java/org/salim/FileUploader.java
@@ -14,13 +14,20 @@ import java.util.List;
 
 public class FileUploader {
     private static final Logger LOG = LoggerFactory.getLogger(FileUploader.class);
-    private static final String remoteRootPath = "/usr/local/filerepo";
-    private static final String localRootPath = "D:\\filerepo\\";
+    private static final String remoteRootPath = "/home/ftpuser/filerepo";
+    private static final String localPcName = "MKT_AIR15";
+    private static final String localFileBase = "/Users/kievjtr/Desktop/FILEREPO";
+
+
 
     private static void checkLocalFiles (List<File> fileList, File file) {
         if (file.isFile()) {
             LOG.info("<<{}>> Is file", file.getName());
-            fileList.add(file);
+            if (!file.getName().startsWith(".")) {
+                fileList.add(file);
+            } else {
+                LOG.info("Ignore this file:{}", file.getName());
+            }
         } else {
             LOG.info("<<{}>> Is directory, go deeper", file.getName());
             for (File nextFile : file.listFiles()) {
@@ -29,15 +36,16 @@ public class FileUploader {
         }
     }
 
-    private static void upload (FTPClient ftpClient, String ftpRootDirectory, File localFile) throws IOException {
-        ftpClient.enterLocalPassiveMode();
-        ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
-
-        if (!ftpClient.changeWorkingDirectory(ftpRootDirectory)) {
-            ftpClient.makeDirectory(ftpRootDirectory);
-            ftpClient.changeWorkingDirectory(ftpRootDirectory);
+    private static void cdOrMkdirCd (FTPClient ftpClient, String path) throws IOException {
+        if (!ftpClient.changeWorkingDirectory(path)) {
+            ftpClient.makeDirectory(path);
+            ftpClient.changeWorkingDirectory(path);
         }
+    }
 
+
+
+    private static void upload (FTPClient ftpClient, File localFile) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(localFile);
 
 
@@ -51,7 +59,7 @@ public class FileUploader {
         ftpClient.setControlEncoding("UTF-8");
         ftpClient.setDataTimeout(120000);
         ftpClient.connect("81.68.160.36");
-        ftpClient.login("ubuntu", "Jfk_181235");
+        ftpClient.login("ftpuser", "JFK193777");
         ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
 
         Integer replyCode = ftpClient.getReplyCode();
@@ -62,26 +70,57 @@ public class FileUploader {
             LOG.info("No Connection");
             return;
         }
-        if (!ftpClient.changeWorkingDirectory(remoteRootPath)) {
-            ftpClient.makeDirectory(remoteRootPath);
-            ftpClient.changeWorkingDirectory(remoteRootPath);
-        }
+        //default work space
+        cdOrMkdirCd(ftpClient, remoteRootPath);
+
+        //find this Local PC folder in remote/ if not, create
+        cdOrMkdirCd(ftpClient, localPcName);
+
+        String remoteBasePath = ftpClient.printWorkingDirectory();
+
 
         //local files
-        File localFileBase = new File(localRootPath);
-        if (localFileBase.exists() && localFileBase.isDirectory()) {
+        File localFileBaseFile = new File(localFileBase);
+        List<File> allLocalFiles = new ArrayList<>();
+        if (localFileBaseFile.exists() && localFileBaseFile.isDirectory()) {
             //遍历，推荐用递归,列出所有文件
-            List<File> allFiles = new ArrayList<>();
-            checkLocalFiles(allFiles, localFileBase);
+            checkLocalFiles(allLocalFiles, localFileBaseFile);
 
-            System.out.println("Fin");
         } else {
             LOG.error("This path not exists or not directory");
         }
 
-        //remote files, last modify date
+        //Go for upload MKdir/upload
+        ftpClient.enterLocalPassiveMode();
 
+        for (File localFile : allLocalFiles) {
+            //first, make dirs
+            String pathUnderBase = localFile.getCanonicalPath();
+            pathUnderBase = pathUnderBase.replace(localFileBase, "");
+            pathUnderBase = pathUnderBase.replace(localFile.getName(), "");
 
+            pathUnderBase = pathUnderBase.replaceAll("\\\\", "/");
+
+            LOG.info("Got file folder path under base: {}", pathUnderBase);
+
+            if (pathUnderBase.startsWith("/")) {
+                cdOrMkdirCd(ftpClient, remoteBasePath + pathUnderBase);
+            } else {
+                cdOrMkdirCd(ftpClient, remoteBasePath + "/" + pathUnderBase);
+            }
+
+            FileInputStream fileInputStream = new FileInputStream(localFile);
+            if (ftpClient.storeFile(localFile.getName(), fileInputStream)) {
+                LOG.info("Uploaded file:{}, success", localFile.getName());
+            } else {
+                LOG.error("Uploaded file:{}, failed", localFile.getAbsolutePath());
+            }
+            fileInputStream.close();
+
+        }
+
+        ftpClient.logout();
+        ftpClient.disconnect();
 
     }
 }
